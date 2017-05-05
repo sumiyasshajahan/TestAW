5db4851ec461d0e6edfe806a9863925ed45d5c4d
sumiyasshajahan <sumiya_shajahan@gensler.com>
2017-05-05 16:38:52 +0530

    testing

---
+++ Tables/dbo.IdentityAW.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[IdentityAW]
+(
+[Gravity] [varchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[uio] [int] NULL
+) ON [PRIMARY]
+GO
---
+++ Tables/dbo.Testo.sql
@@ -0,0 +1,5 @@
+CREATE TABLE [dbo].[Testo]
+(
+[Id] [int] NULL
+) ON [PRIMARY]
+GO
