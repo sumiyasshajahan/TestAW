97b1db1d589313c7f6f1efb672c78515f153b75e
sumiyasshajahan <sumiya_shajahan@gensler.com>
2017-05-09 15:51:39 +0530

    test

--- Tables/dbo.Country.sql
+++ Tables/dbo.Country.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[Country]
 (
 [Id] [int] NULL,
-[Name] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[Name] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[code] [varchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
