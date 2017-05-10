91cc89cc141bf53b4df4dd2b032b81550c2d42fa
sumiyasshajahan <sumiya_shajahan@gensler.com>
2017-05-10 12:39:14 +0530

    update country

--- Tables/dbo.Country.sql
+++ Tables/dbo.Country.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [dbo].[Country]
 (
-[Id] [int] NULL,
+[Id] [int] NOT NULL CONSTRAINT [DefSequence] DEFAULT (NEXT VALUE FOR [country_id_sequence]),
 [Name] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [code] [varchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
