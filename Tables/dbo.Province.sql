13b71a13519840dbf6b3181635ba670f7692d23f
sumiyasshajahan <sumiya_shajahan@gensler.com>
2017-05-10 10:59:20 +0530

    Province

---
+++ Tables/dbo.Province.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[Province]
+(
+[Latitude] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Longitude] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
