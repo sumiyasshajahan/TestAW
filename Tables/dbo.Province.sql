7324a33798ab88ebd70c37861cd54e0af7f7aaa5
sumiyasshajahan <sumiya_shajahan@gensler.com>
2017-05-10 12:41:24 +0530

    table change

--- Tables/dbo.Province.sql
+++ Tables/dbo.Province.sql
@@ -1,6 +1,10 @@
 CREATE TABLE [dbo].[Province]
 (
 [Latitude] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[Longitude] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[Longitude] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[ProvinveId] [int] NOT NULL IDENTITY(1, 1),
+[Id] [int] NULL
 ) ON [PRIMARY]
+ALTER TABLE [dbo].[Province] ADD 
+CONSTRAINT [pk_province] PRIMARY KEY CLUSTERED  ([ProvinveId]) ON [PRIMARY]
 GO
